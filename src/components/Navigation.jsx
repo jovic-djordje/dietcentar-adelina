a1801065ffc2da2c68cc5e6565fc80aa8246ae61
jovic-djordje <jovicdjordje21@gmail.com>
2026-04-11 21:07:56 +0200

    building navigation on homepage

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,7 +1,22 @@
+import { Logo } from "../assets/Images";
 import "./nav.style.css";
 
 const Navigation = () => {
-  return <h1>navigacija</h1>;
+  return (
+    <header>
+      <div className="header-holder">
+        <Logo className="logo" />
+        <nav>
+          <ul className="nav-holder">
+            <li className="nav-item">O centru</li>
+            <li className="nav-item">O meni</li>
+            <li className="nav-item">Usluge</li>
+          </ul>
+        </nav>
+        <button className="nav-btn">Kontakt</button>
+      </div>
+    </header>
+  );
 };
 
 export default Navigation;
