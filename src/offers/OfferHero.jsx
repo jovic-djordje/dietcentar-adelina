c16bc6e92c659c25ae83077c47d4e6030015632f
jovic-djordje <jovicdjordje21@gmail.com>
2026-04-17 20:16:24 +0200

    working on offer page plans section

--- src/offers/OfferHero.jsx
+++ src/offers/OfferHero.jsx
@@ -1,21 +1,16 @@
-import { HiArrowNarrowRight } from "react-icons/hi";
+import { WaveLight } from "../assets/Images";
 
 const OfferHero = () => {
   return (
     <section className="center-hero-section offer-hero-section">
+      <WaveLight className="hero-bg-shape" />
       <div className="center-hero-section-holder offer-hero-section-holder">
         <div className="center-hero-section-text-holder offer-hero-section-text-holder">
           <h1 className="hero-section-title">Odaberi svoj paket usluga</h1>
           <p className="hero-section-text">
-            Svaki program je jedinstven — kao i ti.
+            Svaka usluga je osmišljena kao samostalan korak ili dio dugoročnog
+            procesa — birajte ono što odgovara vašim potrebama.
           </p>
-          <button className="hero-section-btn">
-            Zakaži besplatnu inicijalnu konsultaciju
-            <div className="hero-btn-icon-holder">
-              {" "}
-              <HiArrowNarrowRight className="hero-btn-icon" />
-            </div>
-          </button>
         </div>
       </div>
     </section>
